d426c2b54e7b590ecdd41852dfdbd19b3c7c3e4e
Silke Jandl <silke8421@gmx.at>
2020-03-04 10:18:33 +0100

    bruttonetto

--- src/BruttoNetto.java
+++ src/BruttoNetto.java
@@ -4,9 +4,14 @@ public class BruttoNetto {
         float floatSteuersatz = 0.05f;
         double steuersatz;
         steuersatz = 0.05;
+        double maxsteuersatz = 0.5;
 
         double bruttoPreis;
         bruttoPreis = nettoPreis + nettoPreis * steuersatz;
         System.out.println("Ergebnis: " + bruttoPreis + " €");
+
+        boolean st = steuersatz < maxsteuersatz;
+        System.out.println ("result " + st);
+
     }
 }
